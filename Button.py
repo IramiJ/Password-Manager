887aff5868c55dea14b32e2b422867b6e6dc4142
Colliname <yugiohduel3@gmail.com>
2025-10-03 15:01:54 +0200

    worked on the button function and the layout of the window

--- Button.py
+++ Button.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QPushButton
-
 class button(QPushButton):
- def __init__(self):
-  super().__init__()
+ def __init__(self, width, height, text, mainwindow, func):
+  super().__init__(text, mainwindow)
+  self.setFixedSize(width, height)
+  self.clicked.connect(func)
--- main_window.py
+++ main_window.py
@@ -1,14 +1,20 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget
+from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QPushButton
+from Button import button
+
+
 
 class MainWindow(QMainWindow):
  def __init__(self):
   super().__init__()
   self.initUI()
- 
+
  def initUI(self):
   self.setWindowTitle("Password Manager")
   self.setGeometry(0,0,800,560)
   self.setMaximumHeight(560)
   self.setMaximumWidth(800)
   central_widget = QWidget()
+  grid = QGridLayout()
+  central_widget.setLayout(grid)
   self.setCentralWidget(central_widget)
+#  buttons = [button(50,50,self)]  added a test button to see if everything works
