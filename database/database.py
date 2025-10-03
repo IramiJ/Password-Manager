4140aaacdf7356075e6c6839d608911867867ec0
Colliname <yugiohduel3@gmail.com>
2025-10-03 14:20:53 +0200

    added some more folders and functions

---
+++ database/database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+connection = sqlite3.connect('passwords.db')
+
+def add():
+ pass
+
+def remove():
+ pass
+
+def replace():
+ pass
+
+def view_specific():
+ pass
+
+def view_all():
+ pass
--- main_window.py
+++ main_window.py
@@ -3,12 +3,12 @@ from PyQt5.QtWidgets import QMainWindow, QWidget
 class MainWindow(QMainWindow):
  def __init__(self):
   super().__init__()
-  self.setWindowTitle("Password Manager")
-  self.setGeometry(0,0,450,450)
-  self.setMaximumHeight(450)
-  self.setMaximumWidth(450)
   self.initUI()
  
  def initUI(self):
+  self.setWindowTitle("Password Manager")
+  self.setGeometry(0,0,800,560)
+  self.setMaximumHeight(560)
+  self.setMaximumWidth(800)
   central_widget = QWidget()
   self.setCentralWidget(central_widget)
