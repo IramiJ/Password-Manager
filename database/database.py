9724bf783ee543d1b9e90e454298a3ff0f71410d
Colliname <yugiohduel3@gmail.com>
2025-10-03 21:18:15 +0200

    added the buttons to the mainwindow, aligned them correctly

--- database/database.py
+++ database/database.py
@@ -1,30 +1,36 @@
 import sqlite3
 
-connection = sqlite3.connect('database/test.db')
 
-cursor = connection.cursor()
 
 def add(app_name, password): # adds a new password to the table 
+ connection = sqlite3.connect('database/test.db')
+ cursor = connection.cursor()
  cursor.execute("INSERT INTO passwords VALUES (?, ?) ", (app_name, password))
  connection.commit()
 
 def remove(app_name): # removes a certain password from the table
+ connection = sqlite3.connect('database/test.db')
+ cursor = connection.cursor()
  cursor.execute("DELETE FROM passwords WHERE app_name = ?", (app_name, ))
  connection.commit()
 
 def replace(app_name, password): # allows you to update your password, finding it by app_name
+ connection = sqlite3.connect('database/test.db')
+ cursor = connection.cursor()
  cursor.execute("UPDATE passwords SET password = (?) WHERE app_name = (?)", (password, app_name))
  connection.commit()
 
 def view_specific(app_name): # allows you to view a specific password, finding it by app_name
+ connection = sqlite3.connect('database/test.db')
+ cursor = connection.cursor()
  cursor.execute("SELECT password FROM passwords WHERE app_name = (?)", (app_name, ))
  all = cursor.fetchall()[0][0]
  return all
 
 def view_all(): # shows all of the entrys within the database
+ connection = sqlite3.connect('database/test.db')
+ cursor = connection.cursor()
  cursor.execute("SELECT * FROM passwords")
  all = cursor.fetchall()
+ print(all)
  return all
-
-print(view_specific("app"))
-connection.close()
--- main_window.py
+++ main_window.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QPushButton
+from PyQt5.QtCore import Qt
 from Button import button
-
-
+from database.database import view_all
+from database.database import *
 
 class MainWindow(QMainWindow):
  def __init__(self):
@@ -15,6 +16,20 @@ class MainWindow(QMainWindow):
   self.setMaximumWidth(800)
   central_widget = QWidget()
   grid = QGridLayout()
+  grid.setContentsMargins(0, 0, 0, 0)
+  grid.setSpacing(0)
+  grid.setAlignment(Qt.AlignTop | Qt.AlignLeft)
   central_widget.setLayout(grid)
   self.setCentralWidget(central_widget)
-#  buttons = [button(50,50,self)]  added a test button to see if everything works
+  #added a test button to see if everything works
+  buttons = {
+   'Add_button': button(160, 50, "Add password",self,add),
+   'Delete_button': button(160,50,"Delete password",self,remove),
+   'replace_button': button(160,50,"replace existing password",self,replace),
+   'view_button': button(160,50,'view specific password',self,view_specific),
+   'view_all_button': button(160,50,'view all passwords',self,view_all)
+  }
+  i = 0
+  for b in buttons:
+   grid.addWidget(buttons[b],0,i)
+   i+=1
