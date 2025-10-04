c72dd2fd61d2780a4067937e93cae34934395055
Colliname <yugiohduel3@gmail.com>
2025-10-04 12:23:42 +0200

    started working on a popup window, that is supposed to ask for information

---
+++ popup_window.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QDialog
+
+class popup_window(QDialog):
+ def __init__(self, title, placeholder):
+  super().__init__()
+  self.setFixedSize(300, 210)
+  self.setWindowTitle(title)
+  self.text_field = QLineEdit(self)
+  self.text_field.setPlaceholderText(placeholder)
+  self.text_field.returnPressed.connect(self.return_pressed)
+  self.exec_()
+
+ def return_pressed(self):
+  print("hello")
+  
+
+
