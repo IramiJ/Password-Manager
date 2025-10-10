1763fb55cda4de24a9c76a3d7d1c431c8f23c80d
Colliname <yugiohduel3@gmail.com>
2025-10-10 17:43:39 +0200

    added some further functionality to the top buttons, to make them more responsive

--- database/database.py
+++ database/database.py
@@ -3,24 +3,33 @@ from popup_window import popup_window
 from password_generator import generate_password
 
 def add(mainwindow): # adds a new password to the table 
- connection = sqlite3.connect('database/test.db')
+ connection = sqlite3.connect('database/passwords.db')
  cursor = connection.cursor()
  p1 = popup_window("Enter App Name", "pleace enter your app name")
  app_name = p1.text_field.text()
  password = generate_password(mainwindow.Uppercase, mainwindow.Lowercase, mainwindow.Digits, mainwindow.Symbols, mainwindow.slider.value())
  cursor.execute("INSERT INTO passwords VALUES (?, ?) ", (app_name, password))
  connection.commit()
+ mainwindow.output.setPlainText(f"Successfully added password {app_name}")
 
 def remove(mainwindow): # removes a certain password from the table
- connection = sqlite3.connect('database/test.db')
+ connection = sqlite3.connect('database/passwords.db')
  cursor = connection.cursor()
  p1 = popup_window("Enter App Name", "please enter your app name")
  app_name = p1.text_field.text()
- cursor.execute("DELETE FROM passwords WHERE app_name = ?", (app_name, ))
- connection.commit()
+ cursor.execute("SELECT password FROM passwords WHERE app_name = (?)", (app_name, ))
+ pw = cursor.fetchall()
+ try:
+  pw = pw[0]
+  cursor.execute("DELETE FROM passwords WHERE app_name = ?", (app_name, ))
+  connection.commit()
+  mainwindow.output.setPlainText(f"Successfully deleted password {app_name}")
+ except(IndexError):
+  mainwindow.output.setPlainText(f"Password '{app_name}' does not exist")
+  
 
 def replace(mainwindow): # allows you to update your password, finding it by app_name
- connection = sqlite3.connect('database/test.db')
+ connection = sqlite3.connect('database/passwords.db')
  cursor = connection.cursor()
  p = popup_window("Enter App Name", "please enter your app name")
  app_name = p.text_field.text()
@@ -29,20 +38,21 @@ def replace(mainwindow): # allows you to update your password, finding it by app
  connection.commit()
 
 def view_specific(mainwindow): # allows you to view a specific password, finding it by app_name
- connection = sqlite3.connect('database/test.db')
+ connection = sqlite3.connect('database/passwords.db')
  cursor = connection.cursor()
  p = popup_window("Enter App Name", "please enter your App Name")
  app_name = p.text_field.text()
- cursor.execute("SELECT password FROM passwords WHERE app_name = (?)", (app_name, ))
+ cursor.execute("SELECT password FROM passwords WHERE LOWER(app_name) = LOWER(?)", (app_name, ))
  try:
   all = cursor.fetchall()[0][0]
  except(IndexError):
-  all = ''
- mainwindow.output.setPlainText(all)
+  mainwindow.output.setPlainText(f"App '{app_name}' does not exist")
+  return 0
+ mainwindow.output.setPlainText(f"{app_name}: {all}")
 
 
 def view_all(mainwindow): # shows all of the entrys within the database
- connection = sqlite3.connect('database/test.db')
+ connection = sqlite3.connect('database/passwords.db')
  cursor = connection.cursor()
  cursor.execute("SELECT * FROM passwords")
  all = cursor.fetchall()
